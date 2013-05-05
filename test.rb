9912cc4564ba018403fd800ca9d1efddd3fe4585
Romulo A. Ceccon <romuloceccon@gmail.com>
2013-05-05 18:25:12 -0300

    test boolean options

--- test.rb
+++ test.rb
@@ -7,4 +7,12 @@ class ArgsTestCase < Test::Unit::TestCase
     assert_equal({ count: 100 }, Args.parse(['-n', '100']))
   end
 
+  def test_follow
+    assert_equal({ follow: true }, Args.parse(['-f']))
+  end
+
+  def test_first_line
+    assert_equal({ first_line: true }, Args.parse(['-1']))
+  end
+
 end
